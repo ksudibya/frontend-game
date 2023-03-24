a4747e7fca777975e2397775d82425e40d0557ab
ksudibya <ksudibya@gmail.com>
2023-03-24 11:59:51 -0400

    make an API call

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 
-//set variables for (result, correct answer, incorrect answer, choices, start game, restart game)
 
-let result = document.querySelector('.result')
-let correct =document.querySelectorAll('.correct')
+// let result = document.querySelector('.result')
+// let correct =document.querySelectorAll('.correct')
+
+/**  TO DO
+ 1. call api from opentdb trivia
+ 2. place response from API into question/index
+ 3. create function to pick choices
+ 4. handle result (alert) correct or incorrect
+ 4. save result correct or incorrect
+ 5. show result score
+ */
+const userAction = async () => {
+  
+  const response = await fetch(
+    "https://opentdb.com/api.php?amount=50&difficulty=easy&type=multiple"
+  );
+  const myJson = await response.json(); //extract JSON from the http response
+  
+  console.log(myJson);
+
+   }
+  
+  
