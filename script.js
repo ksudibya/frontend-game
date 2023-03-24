92a513575657501e399758119c5bce34805ea498
ksudibya <ksudibya@gmail.com>
2023-03-23 20:10:10 -0400

    html

---
+++ script.js
@@ -0,0 +1,5 @@
+
+//set variables for (result, correct answer, incorrect answer, choices, start game, restart game)
+
+let result = document.querySelector('.result')
+let correct =document.querySelectorAll('.correct')
